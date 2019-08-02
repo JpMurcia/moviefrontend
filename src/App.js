a722fe90089d255f2d041847b77a4fae67ff2eb8
Juan Camilo Montoya Gonzalez <43051240+JuanCamiloMontoya@users.noreply.github.com>
2019-08-01 19:36:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { sign } from 'crypto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    movieName: "",
+    movieList:[]
+  }
+
+  setMoviename(event) {
+    this.setState({
+      movieName: event.target.value
+    })
+  }
+
+  setLastname(event) {
+    this.setState({
+      lastname: event.target.value
+    })
+  }
+  registerMovie() {
+    const {movieList, movieName} = this.state;
+    this.setState({ movieList: [...movieList, movieName]})
+  }
+
+
+  render() {
+    const { movieName, movieList} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <ul>
+            {
+              movieList.map(movie => {
+                return (
+                  <li>{movie.movieName}saasassa</li>
+                )
+              })
+            }
+          </ul>
+
+
+          <p>{movieName} {movieList}</p>
+          <input type="text" placeholder="Movie name" onChange={(e)=>this.setMoviename(e)}></input>
+          <button type="button" onClick={()=>this.registerMovie()}>Registrar</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
